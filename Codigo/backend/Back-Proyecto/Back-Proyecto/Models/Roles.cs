761d414d2e2e71a3586098ba6a558e9821cb8b65
Anderson Sanchez <anderson.asm201@gmail.com>
2025-11-02 20:42:10 -0500

    creacion de carpetas necesarias

--- Codigo/backend/Back-Proyecto/Back-Proyecto/Models/Roles.cs
+++ Codigo/backend/Back-Proyecto/Back-Proyecto/Models/Roles.cs
@@ -6,9 +6,7 @@ namespace Back_Proyecto.Models
     public class Roles // Roles class representing role entities
 
     {
-        [Key] // Primary Key
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Auto-generate value
-        public Guid Role_Id { get; set; } = Guid.NewGuid(); // Initialize with a new GUID
+        public Guid Role_Id { get; set; } 
         public string Name { get; set; } // Role name
         public string Description { get; set; } // Role description
 
